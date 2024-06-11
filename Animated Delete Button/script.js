83fa542dc748040112f16b8c556231c3262a5d30
ArfinHasib <arfinhasib21375@gmail.com>
2024-06-11 10:22:15 +0600

    Updated

---
+++ Animated Delete Button/script.js
@@ -0,0 +1,9 @@
+document.querySelectorAll('.button').forEach((button) =>
+   button.addEventListener('click', (e) => {
+      if (!button.classList.contains('delete')) {
+         button.classList.add('delete');
+         setTimeout(() => button.classList.remove('delete'), 3200);
+      }
+      e.preventDefault();
+   })
+);
